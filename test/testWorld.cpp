4ffe5fbfeabbf2a62df876055fc2d2dd23ccb23c
cbrantley <cbrantley@cbrantley-laptop.(none)>
2012-02-01 19:02:53 -0800

    Added a test for path selection.  Seems to work great; added makefile for tests, and an additional testmap.  Only need one more test to ensure we are reading in the vectors ok, but might be redundant.

--- test/testWorld.cpp
+++ test/testWorld.cpp
@@ -1,30 +1,84 @@
-#include <vector>
-#include "../src/game/World.h"
-#include "../src/game/WorldPoint.h"
-#include <assert.h>
-#include <iostream>
+#include "testWorld.h"
 
-int main() {
+void testLoop1()
+{
   World world1 ("testWorld1.wf");
 
   WorldPoint current = world1.getCurrent();
-  WorldPoint pnt0 = world1.getAt(0);
-  WorldPoint pnt1 = world1.getAt(1);
-  WorldPoint pnt2 = world1.getAt(2);
-  WorldPoint pnt3 = world1.getAt(3);
   
-  //cout << pnt2.getLeftID() << "\n";
-  assert(pnt2.getLeftID() == 3);
-  assert(pnt1.getLeftID() == 2);
-  
-  {
-    assert(current.getNumberOfIDs() == 1);
-    assert(current.getLeftID() == 1);
-    
-    world1.setChoice(current.getLeftID());
+  for (int i = 1; i < 30; i++) {
     current = world1.getCurrent();
-    
     assert(current.getNumberOfIDs() == 1);
-    assert(current.getLeftID() == 2);
+    assert(current.getLeftID() == i % 5);
+    world1.setChoice(current.getLeftID());
   }
+}
+
+void testPath1()
+{
+  World world2 ("testWorld2.wf");
+  WorldPoint current = world2.getCurrent();
+  int currentIndex = 0;
+  bool checkLeft = false;
+  bool checkMid = false;
+  bool checkRight = false;
+
+  for (int i = 0; i < 100; i++)
+    {
+      current = world2.getCurrent();
+      if (currentIndex == 4) {
+	assert(current.getLeftID() == 5);
+	assert(current.getMiddleID() == 7);
+	assert(current.getRightID() == 10);
+
+	if (!checkLeft) {
+	  checkLeft = true;
+	  currentIndex = 5;
+	  world2.setChoice(currentIndex);
+	}
+	else if (!checkMid) {
+	  checkMid = true;
+	  currentIndex = 7;
+	  world2.setChoice(currentIndex);
+	}
+	else if (!checkRight) {
+	  checkRight = true;
+	  currentIndex = 10;
+	  world2.setChoice(currentIndex);
+	}
+	else {
+	  currentIndex = 10;
+	  world2.setChoice(currentIndex);
+	}
+      }
+      else if (currentIndex == 6) {
+	assert(current.getLeftID() == 0);
+	currentIndex = 0;
+	world2.setChoice(currentIndex);
+      }
+      else if (currentIndex == 9) {
+	assert(current.getLeftID() == 0);
+	currentIndex = 0;
+	world2.setChoice(currentIndex);
+      }
+      else if (currentIndex == 12) {
+	assert(current.getLeftID() == 0);
+	currentIndex = 0;
+	world2.setChoice(currentIndex);
+      }
+      else {
+	currentIndex++;
+	assert(current.getLeftID() == currentIndex);
+	world2.setChoice(currentIndex);
+      }
+      
+    }
+  assert(checkLeft);
+  assert(checkMid);
+  assert(checkRight);
+}
+
+int main() {
+  testLoop1();
+  testPath1();
 }
---
+++ test/testWorld.h
@@ -0,0 +1,13 @@
+#include <vector>
+#include "../src/game/World.h"
+#include "../src/game/WorldPoint.h"
+#include <assert.h>
+#include <iostream>
+
+class testWorld {
+ public:
+  void testLoop1();
+  void testPath1();
+  int main();
+ private:
+};
