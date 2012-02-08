f1411b70bc627c3c542ec3b417eb9d1d63d63915
Tag Ashby <tgashby@calpoly.edu>
2012-02-07 21:49:17 -0800

    Basic Enemy interface

---
+++ src/game/Enemy.cpp
@@ -0,0 +1,16 @@
+//
+//  Enemy.cpp
+//  StarRepub
+//
+//  Created by Taggart Ashby on 2/7/12.
+//  Copyright 2012 476 Proj. All rights reserved.
+//
+
+#include <iostream>
+
+#include "Enemy.h"
+
+Enemy::Enemy(Player& player)
+: m_playerRef(player)
+{
+}
