e152a01585a9ead6687d1487f4403d552ca0f2e7
Tag Ashby <tgashby@calpoly.edu>
2012-02-07 19:34:31 -0800

    Added sounds! Yay!

---
+++ src/engine/Sound.cpp
@@ -0,0 +1,9 @@
+//
+//  MusicTrack.cpp
+//  StarRepub
+//
+//  Created by Taggart Ashby on 2/7/12.
+//  Copyright 2012 476 Proj. All rights reserved.
+//
+
+#include <iostream>
---
+++ src/engine/Sound.h
@@ -0,0 +1,14 @@
+//
+//  MusicTrack.h
+//  StarRepub
+//
+//  Created by Taggart Ashby on 2/7/12.
+//  Copyright 2012 476 Proj. All rights reserved.
+//
+
+#ifndef StarRepub_MusicTrack_h
+#define StarRepub_MusicTrack_h
+
+
+
+#endif
