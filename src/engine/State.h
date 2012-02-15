9caf4f6ffc6e6646d3ad36df22cfcc3227c6d8c7
Tag Ashby <tgashby@calpoly.edu>
2012-02-14 20:25:27 -0800

    Minor Documentation fix

--- src/engine/State.h
+++ src/engine/State.h
@@ -1,7 +1,7 @@
 #pragma once
 
 /**
- * State is an interface for game states
+ * State is an interface for game states.
  * Each state must implement initialize, display, and cleanUp
  */
 struct State
@@ -9,19 +9,19 @@ struct State
    State() { m_initialized = false; }
    
    /**
-    * Initializes anything needed for the given state
+    * Initializes anything needed for the given state.
     * This could be meshes, textures, etc
     */
    virtual void initialize() = 0;
    
    /**
-    * Displays the State
-    * Initialize must be called before this
+    * Displays the State.
+    * Initialize must be called before this.
     */
    virtual void display() = 0;
    
    /**
-    * Cleans up the state, deallocating things, removing things, etc
+    * Cleans up the state, deallocating things, removing things, etc.
     */
    virtual void cleanUp() = 0;
    
